606e0c3ea4e7eea08ddf0f3da0a9b7d1e9018418
Gustav-Frising <frising96@gmail.com>
2024-01-25 11:49:00 +0100

    implemented add new element

--- main.js
+++ main.js
@@ -4,20 +4,24 @@ elementContainer = document.querySelector("#cards");
 
 let idInput = document.querySelector("#id-Input");
 let nameInput = document.querySelector("#name-Input");
-let directorInput = document.querySelector("#genre-Input");
-let genreInput = document.querySelector("#director-Input");
+let genreInput = document.querySelector("#genre-Input");
+let directorInput = document.querySelector("#director-Input");
 let ratingInput = document.querySelector("#rating-Input");
+
+let addElementBtn = document.querySelector("#add-element-btn");
+
+addElementBtn.addEventListener("click", addElement);
 
 function addElement() {
   let newElement = {
     id: Number(idInput.value),
     name: nameInput.value,
     director: directorInput.value,
-    genre: genreinput.value,
+    genre: genreInput.value,
     rating: Number(ratingInput.value),
   };
 
-  fetch("https://localhost:7294/v1/Films_And_Series_", {
+  fetch("http://localhost:5140/v1/Films_And_Series_", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(newElement),
@@ -25,13 +29,13 @@ function addElement() {
     if (res.ok) {
       getElement();
     } else {
-      console.warn("something went wrong!");
+      console.warn("Something is wrong with the API!");
     }
   });
 }
 
 function getElement() {
-  fetch("https://localhost:7294/v1/Films_And_Series_")
+  fetch("http://localhost:5140/v1/Films_And_Series_")
     .then((res) => res.json())
     .then((data) => displayElement(data));
 }
